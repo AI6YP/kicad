3475e8fe7ffe7a4271452df6f2e0db329145340b
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-10-12 00:38:43 -0700

    initial inductor model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kicad
+
 kicad library
+
+## 3D Models
+
+https://github.com/jscad/OpenJSCAD.org
+
+http://happyrobotlabs.com/posts/tutorials/tutorial-3d-kicad-parts-using-openscad-and-wings3d/
---
+++ l-pcb.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const relay = () =>
+    cube([10, 6, 3.90])
+        .setColor([1, 1, 1])
+        .translate([0, 0, 0.75]);
+
+const pcb = () =>
+    cube([40, 40, -1.6])
+        .setColor([0, .3, 0])
+        .translate([-20, -20]);
+
+const relay4 = () =>
+    union(
+        relay().translate([1, 4, 0]),
+        relay().translate([-11, 4, 0]),
+        relay().translate([1, -10, 0]),
+        relay().translate([-11, -10, 0])
+    );
+
+const t200_6 = () =>
+    difference(
+        cylinder({r: 25.5, h: 14}),
+        cylinder({r: 15.5, h: 15})
+    );
+
+const main = () => union(
+    relay4(),
+    pcb(),
+    t200_6().translate([0, 0, -25])
+);
