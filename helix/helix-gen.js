737a923da9f532f0aec9b7ea65d82e4e8e285beb
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2018-01-14 23:44:02 -0800

    new version of helix

--- helix/helix-gen.js
+++ helix/helix-gen.js
@@ -9,9 +9,9 @@ const CAG = scad.csg.CAG;
 
 // BEGIN
 const helixD = 5;
-const turns = 5;
-const pitch = 8 / 5;
-const wireRadius = 0.5;
+const turns = 4.75;
+const pitch = 8 / 4.99;
+const wireRadius = 0.52;
 const angleStep = 5;
 const offset = 0.25 * 360 / angleStep;
 
@@ -23,7 +23,7 @@ const wireRadius3 = wireRadius * Math.sqrt(3) / 2;
 function compass3d (props) {
 	props = props || {};
 	const radius = props.radius || 1;
-	const resolution = props.resolution || 8;
+	const resolution = props.resolution || 16;
 	const adelta = 2 * Math.PI / resolution;
 	let res = [];
 	for(let i = 0; i < resolution; i++) {
@@ -41,33 +41,36 @@ function cageGen (CSG, props) {
     props = props || {};
     const H = props.H || 5;
     const S = props.S || 5;
-    const min = props.min || 0.8;
+    const min = props.min || 1.1;
 
-    const bars = [-9, -8, -7, -6, -5, -4, -3, -2, 2, 3, 4, 5, 6, 7, 8, 9]
-    .map(el => el / Math.PI)
+    const bars = [-7, -6, -5, -4, -3, -2, 2, 3, 4, 5, 6, 7]
+    .map(el => (1.29 * el) / Math.PI)
     .map(el => [S * Math.cos(el), S * Math.sin(el)])
     .reduce((res, el) => {
         return res.union(CSG.roundedCylinder({
             start: [0, el[0], el[1]],
             end: [H, el[0], el[1]],
-            radius: min / 2
+            radius: min / 2,
+            // resolution: 32
         }))
     }, {union: el => el});
 
     const floor = CSG.cylinder({
         start: [0, 0, 0],
         end: [0 - min, 0, 0],
-        radius: S + min / 2
+        radius: S + min / 2,
+        // resolution: 128
     }).subtract(CSG.cylinder({
-        start: [3, -3, 0],
-        end: [-3, -9, 0],
-        radius: 1.5
+        start: [1, -6, 0],
+        end: [-3, -6, 0],
+        radius: 1.4
     }));
 
     const ceiling = CSG.cylinder({
         start: [H, 0, 0],
         end: [H + min, 0, 0],
-        radius: S
+        radius: S + min / 2,
+        // resolution: 128
     }).subtract(CSG.cylinder({
         start: [0, 0, 0],
         end: [H + min + 1, 0, 0],
@@ -79,7 +82,7 @@ function cageGen (CSG, props) {
 
 function helixGen (CSG, props) {
     const cut = CSG.Polygon.createFromPoints(compass3d({
-        resolution: 16,
+        // resolution: 32,
         radius: wireRadius
     }));
     const helix = cut.solidFromSlices({
@@ -116,8 +119,8 @@ const main = () => {
     const core1 = helixGen(CSG).setColor(0, 0, 1);
     const cage1 = cageGen(CSG, {H: 13, S: 5.5}).setColor(0, 1, 1);
     const link = CSG.cube({
-        corner1: [9, -.5, 3],
-        corner2: [13.5, .5, -3]
+        corner1: [9, -.5, 3.6],
+        corner2: [13.5, .5, -3.6]
     });
 	return core0.union(cage0).translate([0, -5, 0])
 	.union(core1.union(cage1).rotateX(180).translate([0, 5, 0]))
