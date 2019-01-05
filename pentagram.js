b4d51b4b732768b8c09deb90927f9a58aebccea2
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-01-05 00:37:33 -0800

    Update pentagram.js

--- pentagram.js
+++ pentagram.js
@@ -45,20 +45,20 @@ const pentagon = s => [0, 1, 2, 3, 4]
 
 function main (props) {
     const a = polygon(pentagram(props.scale))
-        .extrude({offset: [0, 0, props.height]});
+        .extrude({offset: [0, 0, props.height]})
 
     const b = polygon(pentagram(props.scale * (1 - props.wall)))
-        .extrude({offset: [0, 0, props.height + 1]});
+        .extrude({offset: [0, 0, props.height + 1]})
 
     const c = polygon(pentagon(props.scale / 2))
         .extrude({offset: [0, 0, props.height + 1]})
-        .translate([0, 0, props.height / 2]);
+        .translate([0, 0, props.height / 2])
 
     const d = polygon(pentagon(props.scale * pental))
-        .extrude({offset: [0, 0, props.height]});
+        .extrude({offset: [0, 0, props.height]})
 
     const e = polygon(pentagon(props.scale * pental * (1 - props.wall)))
-        .extrude({offset: [0, 0, props.height]});
+        .extrude({offset: [0, 0, props.height]})
         
     const bar = cube({size: [
         props.scale,
@@ -66,21 +66,14 @@ function main (props) {
         3 * props.height
     ], center: true})
         .translate([0, props.scale * 0.05])
-        // .setColor([0, 1, 0])
-
-    const bar1 = cube({size: [
-        props.scale,
-        props.scale * props.wall * 0.1,
-        3 * props.height
-    ]})
-        .translate([0, props.scale * 0.05, props.height / 2])
-        // .setColor([0, 1, 0])
 
     return (
         a
         .subtract(b)
-        .subtract(c)
-        .union(d)
-        .subtract(e.subtract(bar))
+        .setColor([1, 0, 0])
+        .subtract(c.setColor([0, 0, 0]))
+        .union(
+            d.subtract(e.subtract(bar)).setColor([1, 0, 0])
+        )
     );
 }
